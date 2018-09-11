e82faaa631fb2d0e45cdb34f52f93d756f1851e6
Darth Cat <ol-coyote@users.noreply.github.com>
2018-09-10 23:28:12 -0400

    python main template
    Used to start python scripts properly... Why? I'm lazy, what can I say. I'd rather just start with the template.

---
+++ python_template.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3 -tt
+#-*- coding: UTF-8 -*-
+
+"""
+	DOCSTRING: The purpose of this script is to work off of as a template.
+"""
+
+def main():
+	"""
+		DOCSTRING: Do some main stuff
+	"""
+
+if __name__ == '__main__':
+	main()
